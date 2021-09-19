2499c1b201c44aba7e3ebc8ccd97baf192612267
Albert Mayta Segovia <54085998+MSAlbert40@users.noreply.github.com>
2021-09-19 09:19:24 -0500

    Initial project configuration

---
+++ src/test/java/com/evertix/masterregister/MasterRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evertix.masterregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
